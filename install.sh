6d969560856597da08857d0252a1c728ac18bfda
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-23 15:05:05 -0400

    add some vars, fmt, etc

--- install.sh
+++ install.sh
@@ -10,23 +10,26 @@ DISPLAY_NAME='GitHub Desktop'
 APP_NAME='shiftkey-desktop'
 PACK_NAME='github-desktop'
 
-ROOT='https://mirror.mwt.me/ghd/' # USM root URL
+PC_ROOT='https://packagecloud.io/shiftkey/desktop/'
+USM_ROOT='https://mirror.mwt.me/ghd/'
 
-GPG_KEY="${ROOT}gpgkey" # url for the asc gpg key
+GPG_KEY="${USM_ROOT}gpgkey"
 
 OK_MSG='DONE!'
 
 if [ "$1" = "$PC" ]; then
-	DEB_URL='https://packagecloud.io/shiftkey/desktop/any'
+	DEB_URL="${PC_ROOT}any"
+	RPM_URL="${PC_ROOT}el/7/\$basearch"
 elif [ "$1" = "$USM" ]; then
-	DEB_URL="${ROOT}deb"
+	DEB_URL="${USM_ROOT}deb"
+	RPM_URL="${USM_ROOT}rpm"
 elif [ "$1" = "$HELP_CMD" ]; then
 	echo "\
 		$DISPLAY_NAME installer.\
 		sub-commands\
 		$HELP_CMD: prints this text and exits\
-		$PC: uses packagecloud.io repo\
-		$USM: uses mirror.mwt.me repo\
+		$PC: install from packagecloud.io repo\
+		$USM: install from mirror.mwt.me repo\
 	"
 	exit 0
 else
@@ -34,12 +37,10 @@ else
 	exit 1
 fi
 
-# string to appear in the .list file
 DEB_REPO="$DEB_URL any main"
-# string to appear in the .repo file
 RPM_REPO="[shiftkey]
 name=$DISPLAY_NAME
-baseurl=${ROOT}rpm
+baseurl=$RPM_URL
 enabled=1
 gpgcheck=0
 repo_gpgcheck=1
@@ -49,39 +50,55 @@ gpgkey=$GPG_KEY"
 if [ -n "$DEB_REPO" ] && command -v apt >/dev/null; then
 	GPG_KEY_PATH="/usr/share/keyrings/$APP_NAME.asc"
 	APTLIST_PATH="/etc/apt/sources.list.d/$APP_NAME.list"
-	echo "APT detected. Installing APT repository."
+
+	echo 'APT detected. Installing APT repository.'
+
 	if command -v wget >/dev/null; then
 		echo 'Wget detected, using it.'
 		wget -qO "$GPG_KEY_PATH" "$GPG_KEY"
+
 	elif command -v curl >/dev/null; then
 		echo 'cURL detected, using it.'
 		curl -s "$GPG_KEY" -o "$GPG_KEY_PATH"
+
 	else
 		echo 'FAILED: Neither Wget nor cURL available.'
 		exit 1
+
 	fi
+
 	DEBIAN_ARCH=$(dpkg --print-architecture)
 	echo "deb [arch=$DEBIAN_ARCH signed-by=$GPG_KEY_PATH by-hash=force] $DEB_REPO" > "$APTLIST_PATH"
+
 	apt update
 	apt install "$PACK_NAME"
+
 	echo "$OK_MSG"
 	exit 0
+
 elif [ -n "$RPM_REPO" ]; then
+
 	if command -v dnf >/dev/null || command -v yum >/dev/null; then
 		echo 'YUM/DNF detected. Installing YUM repository.'
 		RPMLIST_PATH="/etc/yum.repos.d/$APP_NAME.repo"
+
 	elif command -v zypper >/dev/null; then
 		echo 'Zypper detected. Installing zypper repository.'
 		RPMLIST_PATH="/etc/zypp/repos.d/$APP_NAME.repo"
+
 	else
 		echo 'FAILED: No supported package manager found (apt, yum, dnf, zypper).'
 		exit 1
+
 	fi
+
 	rpm --import "$GPG_KEY"
 	echo "$RPM_REPO" > "$RPMLIST_PATH"
 	echo "$OK_MSG"
 	exit 0
+
 else
 	echo 'FAILED: No supported package manager found.'
 	exit 1
+
 fi
