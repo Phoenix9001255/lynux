33ebb337b35a45cc0481d64008e4dbaba18a2f75
Brendan Forster <github@brendanforster.com>
2023-07-09 20:25:14 -0300

    introduce separate publish step which runs after build

---
+++ script/generate-release-notes.ts
@@ -0,0 +1,69 @@
+import * as glob from 'glob'
+import { basename } from 'path'
+import * as fs from 'fs'
+
+// eslint-disable-next-line no-sync
+const Glob = glob.GlobSync
+
+const args = process.argv.slice(2)
+const artifactsDir = args[0]
+
+const files = new Glob(artifactsDir + '/**/*', { nodir: true })
+let countFiles = 0
+const shaEntries: Array<{ filename: string; checksum: string }> = []
+
+for (const file of files.found) {
+  if (file.endsWith('.sha256')) {
+    shaEntries.push(getShaContents(file))
+  }
+
+  countFiles++
+}
+
+console.log(`Found ${countFiles} files in artifacts directory`)
+console.log(shaEntries)
+
+const draftReleaseNotes = generateDraftReleaseNotes([], shaEntries)
+const releaseNotesPath = __dirname + '/release_notes.txt'
+
+// eslint-disable-next-line no-sync
+fs.writeFileSync(releaseNotesPath, draftReleaseNotes, { encoding: 'utf8' })
+
+console.log(
+  `✅ All done! The release notes have been written to ${releaseNotesPath}`
+)
+
+/**
+ * Returns the filename (excluding .sha256) and its contents (a SHA256 checksum).
+ */
+function getShaContents(filePath: string): {
+  filename: string
+  checksum: string
+} {
+  const filename = basename(filePath).slice(0, -7)
+  // eslint-disable-next-line no-sync
+  const checksum = fs.readFileSync(filePath, 'utf8')
+
+  return { filename, checksum }
+}
+
+/**
+ * Takes the release notes entries and the SHA entries, then merges them into the full draft release notes ✨
+ */
+function generateDraftReleaseNotes(
+  releaseNotesEntries: Array<string>,
+  shaEntries: Array<{ filename: string; checksum: string }>
+): string {
+  const changelogText = releaseNotesEntries.join('\n')
+
+  const fileList = shaEntries.map(e => `**${e.filename}**\n${e.checksum}\n`)
+  const fileListText = fileList.join('\n')
+
+  const draftReleaseNotes = `${changelogText}
+
+## SHA-256 hashes:
+
+${fileListText}`
+
+  return draftReleaseNotes
+}
